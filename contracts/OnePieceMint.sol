8690980ecc5e6bf9851ccd1002c4f022f44654f7
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-03-14 11:39:25 +0100

    Add mapping and local variables

--- contracts/OnePieceMint.sol
+++ contracts/OnePieceMint.sol
@@ -16,5 +16,18 @@ contract OnePieceMint is VRFConsumerBaseV2, ERC721, Ownable, ERC721URIStorage {
 		"https://gateway.pinata.cloud/ipfs/QmcZS7K7PDrW3fNWmbewLpHwiZ5NvjkBWzo1CvjxGqXjsr",
 		"https://gateway.pinata.cloud/ipfs/QmSGodfpsLxkc73P5Tb819UTwYwfdnEQfadMAmvaUz4sdt",
 		"https://gateway.pinata.cloud/ipfs/QmNaskvboHMpnhQsWggcExJfmswKzDFsGN8TLg19zw98D4",
-];
+    ];
+
+    uint256 private s_tokenCounter; // Used to keep track of the number of NFTs being minted
+    VRFCoordinatorV2Interface private i_vrfCoordinator; // Used to store VRF coordinator link
+    uint64 private i_subscriptionId; // Used to store subscription ID from VRF chainlink
+    bytes32 private i_keyHash; // Used to store key hash from VRF chainlink
+    uint32 private i_callbackGasLimit; // Used to specify the gas limit
+
+    mapping(uint256 => address) private requestIdToSender; // allows the contract to keep track of which address made a request
+    mapping(address => uint256) private userCharacter; // enables the contract to associate each user with their selected character
+    mapping(address => bool) public hasMinted; // prevents users from minting multiple NFTs with the same address
+    mapping(address => uint256) public s_addressToCharacter; // allows users to query which character they received based on their address
+
+
 }
